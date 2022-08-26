5e78bdf95f462102bd2e9d780385d323c91da738
Gabriel Medeiros <gabrielmedeirosvino@gmail.com>
2022-08-26 16:06:18 -0300

    mensagens guncionando e arrumar a qebra de linha

--- script.js
+++ script.js
@@ -1,38 +1,87 @@
-let nomePessoa = { name: prompt('Digite seu nome de usuário') }
+let nome = prompt('Digite seu nome de usuário')
+let nomePessoa = { name: nome }
 console.log(nomePessoa)
+let participantes = {
+    from: nomePessoa.name,
+    to: "Todos",
+    text: "entra na sala...",
+    type: "status"
+}
 let arr = []
 
-function entrouNaSala() {
-    let promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomePessoa)
-    promessa.then(pegarNome)
+
+
+
+
+
+function mandarMensagem() {
+
+    
+    const input = document.querySelector('.escrever').value
+    let participantes = {
+        from: nomePessoa.name,
+        to: "Todos",
+        text: input,
+        type: "message"
+    }
+    let promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', participantes)
+    promessa.then(inutil)
+
+    input = ""
+
 }
-entrouNaSala()
 
-function pegarNome(resposta) {
+function online(){
+    let promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomePessoa)
+    promessa.then(inutil)
+}
 
-    let promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants')
+
+function seEstiverPresente(){
+    let promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nomePessoa)
+    promessa.then(inutil)
+}
+
+
+function inutil(resposta) {
+    console.log(resposta)
+}
+online()
+
+function pegarMensagem() {
+    let promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
     promessa.then(pessoaEntrou)
 }
+pegarMensagem()
 
 
+setInterval(pegarMensagem, 5000)
 
-
-
-
-
+setInterval(seEstiverPresente, 5000)
 
 
 function pessoaEntrou(response) {
     arr = response.data
-    console.log(arr)
-    let chegadaPessoa = document.querySelector('.mensagem-cinza')
+    console.log(arr[1].from)
+    let chegadaPessoa = document.querySelector('ul')
 
     for (i = 0; i < arr.length; i++) {
-        if (arr[i].name === nomePessoa.name) {
-            chegadaPessoa.innerHTML += `
-        ${arr[i].name}
-        `
+        
+        if (i === 0) {
+            chegadaPessoa.innerHTML = ""
         }
+        
+        let mensagem = `<li>
+        <div class="${arr[i].type}" id="mensagem${i}">
+            <span class="hora">${arr[i].time}</span> <span class="nome">${arr[i].from}</span ><span class="type">${arr[i].text}</span>
+        </div>
+    </li>`
+
+
+        chegadaPessoa.innerHTML += mensagem
+        const men = document.getElementById(`mensagem${i}`)
+        men.scrollIntoView()
     }
 }
-//criar outra função para colocar na tela
+
+
